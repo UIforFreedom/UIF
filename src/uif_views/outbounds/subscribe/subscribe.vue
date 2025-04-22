9b4c0a959f3ba2fa98209f82e8d052341afcae91
UIF <UIF@uif.org>
2025-04-22 18:44:26 +0000

    auto update

---
+++ src/uif_views/outbounds/subscribe/subscribe.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app-container">
+    <div v-for="item in config.config.subscribe" :key="item['tag']">
+      <subscribe_item :subscribe_item_info="item" />
+    </div>
+
+    <el-button
+      icon="el-icon-circle-plus"
+      style="width: 100%; font-size: 30px"
+      @click="AddNewSubcription"
+      >{{ $translator({ cn: "添加订阅", en: "Add Subscription" }) }}</el-button
+    >
+  </div>
+</template>
+
+<script>
+import subscribe_item from "./item.vue";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "subscribe",
+  props: [],
+  components: { subscribe_item },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.GetUIFConfig();
+  },
+  computed: {
+    ...mapState(["config", "uif"]),
+  },
+
+  methods: {
+    ...mapActions({
+      ApplyCoreConfig: "uif/ApplyCoreConfig",
+      GetUIFConfig: "uif/GetUIFConfig",
+      SaveUIFConfig: "uif/SaveUIFConfig",
+      AddNewSubcription: "uif/AddNewSubcription",
+    }),
+  },
+};
+</script>
